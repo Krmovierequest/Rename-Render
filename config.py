24047b0a1079bc00a7fcd1852925b797803d8ce5
Kdbhaifreefire <116331949+Krmovierequest@users.noreply.github.com>
2024-08-06 19:22:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "12850056")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "15564ec4a1a2cbef87c99a9aa9e40b34")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", ""6656522620:AAGLN6e3rYujnB_T7IhWJt7emZj5biv63mE") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "krbackup") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "kdbhaifreefire")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://kdbhaifreefire:7872278427kd@kdbhaifreefire.sqwqyrn.mongodb.net/?retryWrites=true&w=majority&appName=Kdbhaifreefire")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
